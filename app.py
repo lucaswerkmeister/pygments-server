451d6c93901b0dda96fc9de0681dbc781df44c80
Lucas Werkmeister <mail@lucaswerkmeister.de>
2021-09-19 18:06:00 +0200

    Introduce AnyIO object for overriding stdout
    This object is a rudimentary attempt to support both bytes and str
    writes. With this, `pygmentize -V` works, which is important because
    MediaWiki started to call this command with Gerrit change I2e82e5aa2a
    (wikimedia/mediawiki-extensions-SyntaxHighlight_GeSHi@af6654e5f9).

--- app.py
+++ app.py
@@ -7,6 +7,25 @@ import flask
 import pygments.cmdline
 
 
+class AnyIO(io.IOBase):
+    """A combination of StringIO and BytesIO
+       that supports writing str and bytes."""
+
+    def __init__(self):
+        self.bytes_io = io.BytesIO()
+        self.string_io = io.TextIOWrapper(self.bytes_io)
+
+    def write(self, content):
+        if isinstance(content, str):
+            return self.string_io.write(content)
+        else:
+            return self.bytes_io.write(content)
+
+    def getvalue(self):
+        self.string_io.flush()
+        return self.bytes_io.getvalue()
+
+
 def no_open(file, mode='r', *args, **kwargs):
     """Fake open() function that always return empty objects."""
     if 'b' in mode:
@@ -26,7 +45,7 @@ def index():
     try:
         sys.stdin = io.BytesIO(flask.request.data)
         sys.stdin.buffer = sys.stdin
-        sys.stdout = io.BytesIO()
+        sys.stdout = AnyIO()
         sys.stdout.buffer = sys.stdout
         __builtins__['open'] = no_open
 
